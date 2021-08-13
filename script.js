591bc757e7b40948b138a61b79ea0d013dd345ab
Joseph <j297g594@gmail.com>
2021-08-13 00:36:30 -0600

    Unoptimized basic layout

--- script.js
+++ script.js
@@ -1,147 +1,72 @@
-const screens = document.querySelectorAll(".screen");
-const screensLength = screens.length - 1;
-let currentScreen = 0;
+const slideElements = document.querySelectorAll('.slider');
 
-const btnSubir = document.querySelector(".BtnArriba");
-const btnBajar = document.querySelector(".BtnAbajo");
+const options = {
+  threshold: 0.5,
+};
 
-const btnPantalla = document.querySelectorAll(".BtnPantalla");
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
 
-/* function wheelScroll(e) {
-  if (currentScreen > 0 && e.deltaY < 0) {
-    currentScreen -= 1;
-  } else if (currentScreen < screensLength && e.deltaY > 0) {
-    currentScreen += 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-}
-function screenUp() {
-  if (currentScreen < screensLength) {
-    currentScreen += 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-}
-function screenDown() {
-  if (currentScreen > 0) {
-    currentScreen -= 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-} */
-
-btnBajar.addEventListener("click", function () {
-  if (currentScreen < screensLength) {
-    currentScreen += 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-});
-
-//Ya sé que esta función es una caca, pero de momento la voy a dejar por que tengo pereza, cuando
-//vaya a terminar la página mi primera tarea será refactorizar esta función para we no sea un
-//bucle infinito
-window.setInterval(function () {
-  btnSubir.classList.toggle("Mostrar", currentScreen != 0);
-  btnBajar.classList.toggle("Mostrar", currentScreen < screensLength);
-}, 500);
-
-btnSubir.addEventListener("click", function () {
-  if (currentScreen > 0) {
-    currentScreen -= 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-
-  console.log(currentScreen);
-});
-
-// currentScreen === 1
-//   ? btnSubir.classList.add("Mostrar")
-//   : btnSubir.classList.remove(" Mostrar");
-
-// currentScreen === screensLength
-//   ? btnSubir.classList.remove(" Mostrar")
-//   : btnSubir.classList.add("Mostrar");
-
-window.addEventListener("keydown", function (event) {
-  if (event.code === "ArrowDown" && currentScreen < screensLength) {
-    currentScreen += 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-});
-
-window.addEventListener("keydown", function (event) {
-  if (event.code === "ArrowUp" && currentScreen > 0) {
-    currentScreen -= 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-});
-
-for (let i = 0, n = screensLength + 1; i < n; i++) {
-  btnPantalla[i].addEventListener("click", function () {
-    screens[i].scrollIntoView({ behavior: "smooth" });
-    currentScreen = i;
+    entry.target.classList.add('show');
+    observer.unobserve(entry.target);
   });
-}
+}, options);
 
-// function primeraPantalla(bool) {
-//   if (!bool) {
-//     btnSubir.classList.toggle("Mostrar", currentScreen != 0);
-//   } else {
-//     btnSubir.classList.remove(" Mostrar");
-//   }
-// }
-// function ultimaPantalla(bool) {
-//   if (!bool) {
-//     btnBajar.classList.toggle("Mostrar", currentScreen < screensLength);
-//   } else {
-//     btnBajar.classList.remove(" Mostrar");
-//   }
-// }
-
-/*  0 no hay boton de subir
-    >= 1 hay boton de subir
-
-    <= n - 1 hay boton de bajar
-    n no hay boton de bajar  */
-
-let scrolling = 1; // this variable will hold the previous value, since we land on 0 value the user can only scroll down
-let scrollingDirection = 0; // this variable will hold the
-let scrollingTo = 0;
-window.addEventListener("wheel", function (event) {
-  let isScrollingDown = Math.sign(event.deltaY);
-
-  if (isScrollingDown != scrolling) {
-    prueba(isScrollingDown);
-    scrolling = isScrollingDown;
-  }
+slideElements.forEach((elem) => {
+  observer.observe(elem);
 });
 
-function prueba(b) {
-  if (currentScreen > 0 && b < 0) {
-    currentScreen -= 1;
-  } else if (currentScreen < screensLength && b > 0) {
-    currentScreen += 1;
-  }
-  screens[currentScreen].scrollIntoView({ behavior: "smooth" });
+const consoleElement_1 = document.querySelector('.console-1');
+const consoleElement_2 = document.querySelector('.console-2');
+const consoleElement_3 = document.querySelector('.console-3');
+const consoleElement_4 = document.querySelector('.console-4');
+const consoleElement_5 = document.querySelector('.console-5');
+const consoleElement_6 = document.querySelector('.console-6');
+const consoleElement_7 = document.querySelector('.console-7');
+const consoleElement_8 = document.querySelector('.console-8');
+const consoleElement_9 = document.querySelector('.console-9');
+const consoleElement_10 = document.querySelector('.console-10');
+const consoleElement_11 = document.querySelector('.console-11');
+const consoleElement_12 = document.querySelector('.console-12');
+const consoleElement_13 = document.querySelector('.console-13');
+const consoleElement_14 = document.querySelector('.console-14');
+const consoleElement_15 = document.querySelector('.console-15');
+const consoleElement_16 = document.querySelector('.console-16');
+const consoleElement_17 = document.querySelector('.console-17');
+
+function shortTimeout(query, timeout) {
+  setTimeout(() => {
+    query.classList.remove('prueba');
+  }, timeout);
 }
 
-// Creo que la solución es la siguiente, crear una condición que compare antes de ejecutar la función
-// utilizamos la variable isScrollingDown, esa variable será global, su valor se asignará dinamicamente
-// si el valor nuevo valor de isScrollingDown es diferente al anterior, no se ejucuta nada y se sale
-// si es diferente si ejecuta, de esa forma solo la ejecutamos una vez
+function longTimeout(query) {
+  setTimeout(() => {
+    query.classList.add('prueba');
+  }, 800);
+}
 
-// window.addEventListener("wheel", function (event) {
-//   if (currentScreen > 0 && event.deltaY < 0) {
-//     currentScreen -= 1;
-//   } else if (currentScreen < screensLength && event.deltaY > 0) {
-//     currentScreen += 1;
-//   }
-//   screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-// });
+const body = document.querySelector('body');
 
-// window.addEventListener("scroll", function (event) {
-//   if (currentScreen > 0 && event.deltaY < 0) {
-//     currentScreen -= 1;
-//   } else if (currentScreen < screensLength && event.deltaY > 0) {
-//     currentScreen += 1;
-//   }
-//   screens[currentScreen].scrollIntoView({ behavior: "smooth" });
-// });
+window.addEventListener('load', () => {
+  shortTimeout(consoleElement_1, 300);
+  longTimeout(consoleElement_2);
+  shortTimeout(consoleElement_3, 1200);
+  shortTimeout(consoleElement_4, 1500);
+  shortTimeout(consoleElement_5, 1800);
+  shortTimeout(consoleElement_6, 1900);
+  shortTimeout(consoleElement_7, 2000);
+  shortTimeout(consoleElement_8, 2100);
+  shortTimeout(consoleElement_9, 2400);
+  shortTimeout(consoleElement_10, 2900);
+  shortTimeout(consoleElement_11, 3200);
+  shortTimeout(consoleElement_12, 3300);
+  shortTimeout(consoleElement_13, 3800);
+  shortTimeout(consoleElement_14, 3900);
+  shortTimeout(consoleElement_15, 4000);
+  shortTimeout(consoleElement_16, 4300);
+  shortTimeout(consoleElement_17, 5000);
+});
